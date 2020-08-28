6addc03419c09af01a5e2b147db4d97788759154
longyuan <huangyong@hifive.ai>
2020-08-28 18:53:58 +0800

    通过运营组获取歌单列表

--- src/main/java/com/hifive/api/DefaultClientTest.java
+++ src/main/java/com/hifive/api/DefaultClientTest.java
@@ -13,7 +13,7 @@ public class DefaultClientTest {
         String secret = "2d241e8f934d47d5";
         System.out.println(System.currentTimeMillis());
         HifiveClient client = new DefaultHifiveClient(url, appkey, secret);
-        OrderGroupSheetRequest(client);
+        OrderSheetMusicRequest(client);
     }
 
     private static void UserGetRequest(HifiveClient client) {
@@ -476,6 +476,35 @@ public class DefaultClientTest {
 
         try {
             HifiveOrderGroupSheetResponse response = client.execute(request);
+            System.out.println(JSON.toJSON(response));
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void TrafficSheetMusicRequest(HifiveClient client) {
+        HifiveTrafficSheetMusicRequest request = new HifiveTrafficSheetMusicRequest();
+        request.setMethod(HifiveUserGetRequest.METHOD_GET);
+        request.setClientId("1234567");
+        request.setToken("394427b702825d59222c71d15ceab720");
+        request.setSheetId(1203L);
+        try {
+            HifiveTrafficSheetMusicResponse response = client.execute(request);
+            System.out.println(JSON.toJSON(response));
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void OrderSheetMusicRequest(HifiveClient client) {
+        HifiveOrderSheetMusicRequest request = new HifiveOrderSheetMusicRequest();
+        request.setMethod(HifiveUserGetRequest.METHOD_GET);
+        request.setClientId("1234567");
+        request.setToken("394427b702825d59222c71d15ceab720");
+        request.setSheetId(1203L);
+        try {
+            HifiveOrderSheetMusicResponse response = client.execute(request);
             System.out.println(JSON.toJSON(response));
         } catch (ApiException e) {
             e.printStackTrace();
---
+++ src/main/java/com/hifive/api/response/HifiveOrderSheetMusicResponse.java
@@ -0,0 +1,28 @@
+package com.hifive.api.response;
+
+import com.hifive.api.HifiveResponse;
+import com.hifive.api.domain.common.HifiveMusicInfoDetail;
+import com.hifive.api.domain.common.HifivePage;
+import com.hifive.api.internal.mapping.ApiField;
+
+/**
+ * 分页。
+ *
+ * @author yong.huang
+ * @since 1.0, Sep 12, 2009
+ */
+public class HifiveOrderSheetMusicResponse extends HifiveResponse {
+
+    @ApiField("data")
+    HifivePage<HifiveMusicInfoDetail> hifivePage;
+
+    public HifivePage<HifiveMusicInfoDetail> getHifivePage() {
+        return hifivePage;
+    }
+
+    public void setHifivePage(HifivePage<HifiveMusicInfoDetail> hifivePage) {
+        this.hifivePage = hifivePage;
+    }
+
+
+}
